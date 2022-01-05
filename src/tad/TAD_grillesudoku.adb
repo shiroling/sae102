14125c199fe5d881911f940649733c81740d8dce
shiroling <contact.shiroling@gmail.com>
2022-01-05 17:27:05 +0100

    3rd

--- src/tad/TAD_grillesudoku.adb
+++ src/tad/TAD_grillesudoku.adb
@@ -6,39 +6,41 @@ package body TAD_grilleSudoku is
    ----------------------
 
    function construireGrille return Type_Grille is
-   g : Type_Grille;
+   grille : Type_Grille;
    begin
-      g :=  ((others=>0),(others=>0),(others=>0),
+      grille :=  ((others=>0),(others=>0),(others=>0),
                   (others=>0),(others=>0),(others=>0),
                   (others=>0),(others=>0),(others=>0));
-      return g;
+      return grille;
    end construireGrille;
 
    --------------
    -- caseVide --
    --------------
 
-   function caseVide (g : in Type_Grille; c : in Type_Coordonnee) 
+   function caseVide (g : in Type_Grille; c : in Type_Coordonnee)
                         return Boolean is
    begin
       if g(obtenirLigne(c),obtenirColonne(c))=0 then
          return true;
       else
-         return flase;
+         return False;
+      end if;
    end caseVide;
 
    --------------------
    -- obtenirChiffre --
    --------------------
 
-   function obtenirChiffre (g : in Type_Grille; c : in Type_Coordonnee) 
+   function obtenirChiffre (g : in Type_Grille; c : in Type_Coordonnee)
                               return Integer is
 
    begin
          if g(obtenirLigne(c),obtenirColonne(c))=0 then
             raise OBTENIR_CHIFFRE_NUL;
          else
-            return g(obtenirLigne(c),obtenirColonne(c));
+         return g(obtenirLigne(c),obtenirColonne(c));
+      end if;
    end obtenirChiffre;
 
    --------------------
@@ -46,21 +48,22 @@ package body TAD_grilleSudoku is
    --------------------
 
    function nombreChiffres (g : in Type_Grille) return Integer is
-      c : Type_Coordonnee := (1,1);
-      cp := 0;
+      c : Type_Coordonnee;
+      colonne : integer := 1;
+      ligne : integer := 1;
+      cp :integer := 0;
    begin
-       while obtenirLigne(c)<=9 loop
-           while obtenirColonne(c)<=9 loop
-               if caseVide(g,c)=false loop
-                   cp := cp =1;
-               end if;
-               obtenirColonne(c):=obtenirColonne+1;
-           end loop;
-           obtenirLigne(c):=obtenirLigne(c)+1;
-           obtenirColonne:=0;
-       end loop;
-       return cp;
+      for i in 1..9 loop
+         for j in 1..9 loop
+            c := construireCoordonnees(i, j);
+            if not caseVide(g, c) then
+               cp := cp+1;
+            end if;
+         end loop;
+      end loop;
+      return cp;
    end nombreChiffres;
+
 
    ------------------
    -- fixerChiffre --
@@ -72,6 +75,7 @@ package body TAD_grilleSudoku is
          raise FIXER_CHIFFRE_NON_NUL;
       else
          g(obtenirLigne(c),obtenirColonne(c)):=v;
+      end if;
    end fixerChiffre;
 
    ---------------
@@ -84,6 +88,7 @@ package body TAD_grilleSudoku is
          raise VIDER_CASE_VIDE;
       else
          g(obtenirLigne(c),obtenirColonne(c)):=0;
+      end if;
    end viderCase;
 
    ----------------
@@ -96,20 +101,23 @@ package body TAD_grilleSudoku is
          return true;
       else
          return false;
+         end if;
    end estRemplie;
 
    ------------------------------
    -- obtenirChiffresDUneLigne --
    ------------------------------
 
-   function obtenirChiffresDUneLigne (g : in Type_Grille; numLigne : in Integer) 
-                                       return Type_Ensemble is
+   function obtenirChiffresDUneLigne (g : in Type_Grille; numLigne : in Integer)
+                                      return Type_Ensemble is
+      e : Type_Ensemble;
    begin
       for i in 1 .. 9 loop
          if g(numLigne, i) /= 0 then
-            ajouterChiffre(g(numLigne, i);
+            ajouterChiffre(e, g(numLigne, i));
          end if;
       end loop;
+      return e;
    end obtenirChiffresDUneLigne;
 
    --------------------------------
@@ -118,12 +126,14 @@ package body TAD_grilleSudoku is
 
    function obtenirChiffresDUneColonne
      (g : in Type_Grille; numColonne : in Integer) return Type_Ensemble is
-      begin
+      e : Type_Ensemble;
+   begin
          for i in 1 .. 9 loop
-            if g(i, numLigne) /= 0 then
-               ajouterChiffre(g(i, numLigne);
+            if g(i, numColonne) /= 0 then
+               ajouterChiffre(e, g(i, numColonne));
             end if;
-         end loop;
+      end loop;
+      return e;
       end obtenirChiffresDUneColonne;
 
    -----------------------------
@@ -132,15 +142,17 @@ package body TAD_grilleSudoku is
 
    function obtenirChiffresDUnCarre (g : in Type_Grille; numCarre : in Integer) return Type_Ensemble is
       cord: Type_Coordonnee;
-   
+      e : Type_Ensemble;
    begin
       cord := obtenirCoordonneeCarre(numCarre);
       for i in 0 .. 2 loop
-         for j in 0 .. 0 loop
-            if g(i + cord.linge, j +) /= 0 then
-               ajouterChiffre(g(i, numLigne);
+         for j in 0 .. 2 loop
+            if g(i + obtenirLigne(cord), j + obtenirColonne(cord)) /= 0 then
+               ajouterChiffre(e, g(i, numCarre));
+            end if;
          end loop;
       end loop;
+      return e;
    end obtenirChiffresDUnCarre;
 
 end TAD_grilleSudoku;
